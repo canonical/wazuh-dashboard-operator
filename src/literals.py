ef3d7fb095384b2f553eaff8cf79782d986484f8
Judit Novak <judit.novak@canonical.com>
2024-07-29 13:04:39 +0200

    Using latest snap version (pointing to the DP kibana-exporter PPA)

--- src/literals.py
+++ src/literals.py
@@ -4,7 +4,7 @@
 
 """Collection of global literals for the charm."""
 
-OPENSEARCH_DASHBOARDS_SNAP_REVISION = "18"
+OPENSEARCH_DASHBOARDS_SNAP_REVISION = "19"
 
 SUBSTRATE = "vm"
 CHARM_KEY = "opensearch-dashboards"
