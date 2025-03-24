f3860fe655fb409761cd3b69a53e9994b2263093
Arturo Seijas <arturo.seijas@canonical.com>
2025-03-24 17:10:47 +0100

    Add terraform module

---
+++ terraform/charm/outputs.tf
@@ -0,0 +1,20 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+output "app_name" {
+  description = "Name of the deployed application."
+  value       = juju_application.wazuh_dashboard.name
+}
+
+output "requires" {
+  value = {
+    certificates      = "certificates"
+    opensearch_client = "opensearch-client"
+  }
+}
+
+output "provides" {
+  value = {
+    cos_agent = "cos_agent"
+  }
+}
