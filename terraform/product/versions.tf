762080bb207d2b6ffa9460bf7ecf6b3bb6b85d51
Arturo Seijas <arturo.seijas@canonical.com>
2025-04-28 10:02:03 +0200

    Update versions

--- terraform/product/versions.tf
+++ terraform/product/versions.tf
@@ -2,12 +2,11 @@
 # See LICENSE file for licensing details.
 
 terraform {
-  required_version = ">= 1.7.2"
+  required_version = ">=0.17.2"
   required_providers {
     juju = {
-      source                = "juju/juju"
-      version               = ">= 0.17.1"
-      configuration_aliases = [juju.wazuh_indexer, juju.wazuh_dashboard]
+      source  = "juju/juju"
+      version = ">= 0.17.1"
     }
   }
 }
