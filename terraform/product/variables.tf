4f01a520e03bfb949b6520e56583ed59aa76937a
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2025-04-29 11:29:35 +0200

    Deploy grafana agent as part of the TF product (#20)

---
+++ terraform/product/variables.tf
@@ -0,0 +1,28 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+variable "dashboard_model" {
+  description = "Reference to the VM Juju model to deploy the dashboard charms to."
+  type        = string
+}
+
+variable "grafana_agent" {
+  type = object({
+    app_name    = optional(string, "grafana-agent")
+    channel     = optional(string, "latest/stable")
+    config      = optional(map(string), {})
+    revision    = optional(number)
+  })
+}
+
+variable "wazuh_dashboard" {
+  type = object({
+    app_name    = optional(string, "wazuh-dashboard")
+    channel     = optional(string, "4.9/edge")
+    config      = optional(map(string), {})
+    constraints = optional(string, "arch=amd64")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@22.04")
+    units       = optional(number, 3)
+  })
+}
