cdd38d2041b72633cf928a1422306715da5b4feb
Sebastien Georget <sebastien.georget@canonical.com>
2025-06-16 18:03:39 +0200

    Fix process name

--- tests/integration/ha/helpers.py
+++ tests/integration/ha/helpers.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
-PROCESS = "/snap/wazuh-dashboard/8/opt/opensearch-dashboards/start.sh"
+PROCESS = "/snap/wazuh-dashboard/3/opt/wazuh-dashboard/start.sh"
 DB_PROCESS = "org.opensearch.bootstrap.OpenSearch"
 SERVICE_DEFAULT_PATH = "/etc/systemd/system/snap.charmed-zookeeper.daemon.service"
 PEER = "cluster"
