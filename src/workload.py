0c4dc4379b2c285be44d781b31482ffb933d99b3
Sébastien Georget <sebastien.georget@canonical.com>
2025-07-17 22:09:32 +0200

    Merge upstream changes (#29)

--- src/workload.py
+++ src/workload.py
@@ -143,7 +143,7 @@ class ODWorkload(WorkloadBase):
             dashboards.ensure(
                 snap.SnapState.Present,
                 revision=OPENSEARCH_DASHBOARDS_SNAP_REVISION,
-                channel="4/edge",
+                channel="4.11/edge",
             )
 
             self.dashboards = dashboards
