754adffa2337f3f65ee2136648af99e0e5123e06
Sebastien Georget <sebastien.georget@canonical.com>
2025-07-17 10:22:36 +0200

    Validate wazuh product

--- terraform/product/variables.tf
+++ terraform/product/variables.tf
@@ -18,7 +18,7 @@ variable "grafana_agent" {
 variable "wazuh_dashboard" {
   type = object({
     app_name    = optional(string, "wazuh-dashboard")
-    channel     = optional(string, "4.9/edge")
+    channel     = optional(string, "4.11/edge")
     config      = optional(map(string), {})
     constraints = optional(string, "arch=amd64")
     revision    = optional(number)
